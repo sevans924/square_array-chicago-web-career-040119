c61760c90708ef37f8f93fb0b5a9d93d62b2899b
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 03:16:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
 
-  square_array = array.each do |i|
+  square_array = []
+  array.each do |i|
     i ** 2
+    square_array.push
   end
     return square_array
   # your code here
